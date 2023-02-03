16111c56f010beddaf8e916a4aba58e1411b066e
Sebastian Vargaz <juancbastianrv@gmail.com>
2023-02-03 18:07:28 -0500

    email test

--- pqrs.php
+++ pqrs.php
@@ -18,15 +18,48 @@ if (isset($_POST['PQR'])) {
    $response = "<script> enviado(); </script>";
 }
 
-ini_set( 'display_errors', 1 );
-error_reporting( E_ALL );
-$from = "isistemas@automarcol.com";
-$to = "juancbastianrv@gmail.com";
-$subject = "Checking PHP mail";
-$message = "PHP mail works just fine";
-$headers = "From:" . $from;
-mail($to,$subject,$message, $headers);
-echo "The email message was sent.";
+$to = 'juancbastianrv@gmail.com'; 
+$from = 'isistemas@automarcol.com'; 
+$fromName = 'Automarcol'; 
+ 
+$subject = "Send HTML Email in PHP by CodexWorld"; 
+ 
+$htmlContent = ' 
+    <html> 
+    <head> 
+        <title>Welcome to CodexWorld</title> 
+    </head> 
+    <body> 
+        <h1>Thanks you for joining with us!</h1> 
+        <table cellspacing="0" style="border: 2px dashed #FB4314; width: 100%;"> 
+            <tr> 
+                <th>Name:</th><td>CodexWorld</td> 
+            </tr> 
+            <tr style="background-color: #e0e0e0;"> 
+                <th>Email:</th><td>contact@codexworld.com</td> 
+            </tr> 
+            <tr> 
+                <th>Website:</th><td><a href="http://www.codexworld.com">www.codexworld.com</a></td> 
+            </tr> 
+        </table> 
+    </body> 
+    </html>'; 
+ 
+// Set content-type header for sending HTML email 
+$headers = "MIME-Version: 1.0" . "\r\n"; 
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+ 
+// Additional headers 
+$headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n"; 
+$headers .= 'Cc: welcome@example.com' . "\r\n"; 
+$headers .= 'Bcc: welcome2@example.com' . "\r\n"; 
+ 
+// Send email 
+if(mail($to, $subject, $htmlContent, $headers)){ 
+    echo 'Email has sent successfully.'; 
+}else{ 
+   echo 'Email sending failed.'; 
+}
 ?>
 <style>
 .form-control {
@@ -35,12 +68,15 @@ echo "The email message was sent.";
     border-radius: 5px;
     font-size: 15px;
 }
+
 .formulario {
     max-width: 60rem;
 }
+
 #pop-up-message-circle {
     background-color: var(--main) !important;
 }
+
 .bg-dark {
     background-color: #2c3b6d !important;
 }
